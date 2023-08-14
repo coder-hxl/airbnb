bbc090a131be0a589f3a28c35382f74e874242cd
coderhxl <coderhxl@outlook.com>
2023-08-14 18:01:52 +0800

    更换后端 IP

--- src/services/request/config.ts
+++ src/services/request/config.ts
@@ -1,4 +1,4 @@
 // export const BASE_URL = 'http://codercba.com:1888/airbnb/api'
-// export const BASE_URL = 'http://8.210.98.225:9001/api'
-export const BASE_URL = 'http://localhost:9001/api'
+// export const BASE_URL = 'http://47.113.218.204:9001/api'
+export const BASE_URL = 'http://47.113.218.204:9001/api'
 export const TIMEOUT = 10000
